f3df992a9e05f255070f65522fae1cccd802038d
Henriquedbrito <rm98831@fiap.com.br>
2023-09-11 19:05:56 -0300

    arrumado os coiso

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import Cabecalho from "./assets/components/Cabecalho";
-import Conteudo from "./assets/components/Conteudo";
-import Rodape from "./assets/components/Rodape"
-import
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+import reactLogo from "./assets/react.svg";
+import { Outlet } from "react-router-dom";
 
 export default function App() {
 
- 
-  
+  let reactLogoAlt = "React Logo";
+
+  //Lista de links redes sociais
+
+  let listaLinks =[<li>Github</li>, <li>X</li>, <li>Reddit</li> ];
+
   return (
     <>
       <div className="container">
 
-       <Cabecalho/>
-        
-       <Conteudo/>
+        <Cabecalho/>
 
-       <Rodape/>
+        {/* <img src={reactLogo} alt={reactLogoAlt} /> */}
+
+        {/* <Conteudo reactLogoProps={reactLogo} reactLogoAltProps={reactLogoAlt} /> */}
+        <Outlet/>
+        <Rodape listaProps={listaLinks}/>
 
       </div>
-    
     </>
   )
 }
